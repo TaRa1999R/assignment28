2deffea3e6aa675ce0cf8e3aa073ebf452254506
TaRa1999R <rezaietara78@gmail.com>
2023-12-09 14:42:30 +0330

    assignment28

--- 1.py
+++ 1.py
@@ -2,9 +2,14 @@
 import cv2
 import numpy as np
 
-football_pitch = cv2.imread ("input images\input_3.jpg")
-# np.ones ((360 , 510) , dtype= np.uint8) * 255
-light_green = cv2.rectangle(football_pitch, (10 , 10) , (500 , 350) , (0 , 255 , 0) , 2)
+football_pitch = cv2.imread ("input images\input_1.jpg")
+light_green = cv2.rectangle (football_pitch, (0 , 0) , (532 , 382) , (0 , 200 , 83) , -1)
+start_point = 76
+for i in range (3) :
+    dark_green = cv2.rectangle (football_pitch , (start_point , 0) , (start_point + 76 , 382) , (46 , 125 , 50) , -1)
+    start_point += 150
+
 
 cv2.imshow ("Football Pitch" , football_pitch)
 cv2.waitKey()
+cv2.imwrite ("output images\outout_1.jpg" , football_pitch)
