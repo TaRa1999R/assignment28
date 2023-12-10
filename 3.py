81c77a0cf8ed84166e79d0604b10a2464ade74d8
TaRa1999R <rezaietara78@gmail.com>
2023-12-10 10:41:07 +0330

    assignment28

--- 3.py
+++ 3.py
@@ -7,6 +7,21 @@ while True :
     _ , fram = cap.read ()
 
     cv2.imshow ("Webcam Filter" , fram)
-    if cv2.waitKey (25) & 0xFF == ord("q") :
-        # cv2.imwrite ("output_images\outout_3.jpg" , fram)
+    if cv2.waitKey (25) & 0xFF == ord ("1") :
+        print ("one")
+        # cv2.imwrite ("output_images\outout_3_stiker.jpg" , fram)
+
+    if cv2.waitKey (25) & 0xFF == ord ("2") :
+        print("two")
+        # cv2.imwrite ("output_images\outout_3_glasses&lips.jpg" , fram)
+
+    if cv2.waitKey (25) & 0xFF == ord ("3") :
+        print ("three")
+        # cv2.imwrite ("output_images\outout_3_chessboard.jpg" , fram)
+
+    if cv2.waitKey (25) & 0xFF == ord ("4") :
+        print ("four")
+        # cv2.imwrite ("output_images\outout_3_mirror.jpg" , fram)
+
+    if cv2.waitKey (25) & 0xFF == ord ("q") :
         break
