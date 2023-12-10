9aaa24059ead082ce096b76036b43c60f32ff821
TaRa1999R <rezaietara78@gmail.com>
2023-12-10 18:45:29 +0330

    assignment28

--- 3.py
+++ 3.py
@@ -5,6 +5,18 @@ cap = cv2.VideoCapture (0)
 
 while True :
     _ , fram = cap.read ()
+    gray_fram = cv2.cvtColor (fram , cv2.COLOR_RGB2GRAY)
+
+    face_detectoe = cv2.CascadeClassifier (cv2.data.haarcascades + "haarcascade_frontalface_alt.xml")
+    faces = face_detectoe.detectMultiScale (gray_fram , scaleFactor = 1.3)
+    eye_detector = cv2.CascadeClassifier (cv2.data.haarcascades + "haarcascade_eye.xml")
+    eyes = eye_detector.detectMultiScale (gray_fram , scaleFactor = 1.3)
+    lip_detector = cv2.CascadeClassifier (cv2.data.haarcascades + "haarcascade_smile.xml")
+    lips = lip_detector.detectMultiScale (gray_fram , scaleFactor = 1.3)
+
+    for face in lips :
+        x , y , w , h = face
+        cv2.rectangle (fram , (x , y) , (x + w , y + h) , (150 , 0 , 150) , 2)
 
     cv2.imshow ("Webcam Filter" , fram)
     if cv2.waitKey (25) & 0xFF == ord ("1") :
