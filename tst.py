4686345664916a1666726d634d60461216c37c17
TaRa1999R <rezaietara78@gmail.com>
2023-12-19 23:17:06 +0330

    assignment28

--- tst.py
+++ tst.py
@@ -2,25 +2,36 @@
 import cv2
 import numpy as np
 
-def Checkered_face () :
-    ...
-
-
 def stiker_face (img , faces) :
     for face in faces :
-        xf,yf,wf,hf = face
-        emoji_small = cv2.resize (emoji_stiker , [wf , hf])
+        xf , yf , wf , hf = face
+        small_emoji = cv2.resize (emoji_stiker , [wf , hf])
         
         for i in range (hf) :
             for j in range (wf) :
-                if emoji_small[i][j][0] == 0 and emoji_small[i][j][1] == 0 and emoji_small[i][j][2] == 0 :
-                    emoji_small[i][j] = img[yf + i , xf + j]
+                if small_emoji[i][j][0] == 0 and small_emoji[i][j][1] == 0 and small_emoji[i][j][2] == 0 :
+                    small_emoji[i][j] = img[yf + i , xf + j]
 
-        img[yf : yf + hf , xf : xf + wf] = emoji_small
+        img [yf : yf + hf , xf : xf + wf] = small_emoji
     return img
     
 
-def lip_and_eye () :
+def lip_and_eye (img , eyes , lips) :
+    for lip in lips :
+        xl , yl , wl , hl = lip
+        small_lip = cv2.resize (lip_stiker , [wl , hl])
+
+        for i in range (hl) :
+            for j in range (wl) :
+                if small_lip[i][j][0] == 0 and small_lip[i][j][1] == 0 and small_lip[i][j][2] == 0 :
+                    small_lip[i][j] = img [yl + i , xl + j]
+        
+        img [yl : yl + hl , xl : xl + wl] = small_lip
+    
+    return img
+
+
+def Checkered_face () :
     ...
 
 
@@ -44,34 +55,30 @@ while True :
 
     faces = face_detectoe.detectMultiScale (gray_fram , scaleFactor = 1.3)
     eyes = eye_detector.detectMultiScale (gray_fram , scaleFactor = 1.3 , minNeighbors = 20)
-    lips = lip_detector.detectMultiScale (gray_fram , scaleFactor = 1.3 , minNeighbors = 40)
+    lips = lip_detector.detectMultiScale (gray_fram , scaleFactor = 1.3 , minNeighbors = 45)
 
-    for eye in eyes :
-        xe,ye,we,he = eye
-        cv2.rectangle (fram , (xe , ye) , (xe + we , ye + he) , (0 , 0 , 150) , 2)
+    # for eye in eyes :
+        # xe,ye,we,he = eye
+        # cv2.rectangle (fram , (xe , ye) , (xe + we , ye + he) , (0 , 0 , 150) , 2)
 
-    for lip in lips :
-        xl,yl,wl,hl = lip
-        cv2.rectangle (fram , (xl , yl) , (xl + wl , yl + hl) , (150 , 0 , 0) , 2)
 
     if cv2.waitKey (25) & 0xFF == ord ('1') :
         fram = stiker_face (fram , faces)
-        cv2.imwrite ("output_images\outout_3_emoji.jpg" , fram)
+        # cv2.imwrite ("output_images\outout_3_emoji.jpg" , fram)
 
     elif cv2.waitKey (25) & 0xFF == ord ('2') :
-        print ("two")
-        fram = lip_and_eye ()
-        cv2.imwrite ("output_images\outout_3_glasses&lips.jpg" , fram)
+        fram = lip_and_eye (fram , eyes , lips)
+        # cv2.imwrite ("output_images\outout_3_glasses&lips.jpg" , fram)
 
     elif cv2.waitKey (25) & 0xFF == ord ('3') :
         print ("three")
         fram = Checkered_face ()
-        cv2.imwrite ("output_images\outout_3_checkred_face.jpg" , fram)
+        # cv2.imwrite ("output_images\outout_3_checkred_face.jpg" , fram)
 
     elif cv2.waitKey (25) & 0xFF == ord ('4') :
         print ("four")
         fram = mirror ()
-        cv2.imwrite ("output_images\outout_3_mirror.jpg" , fram)
+        # cv2.imwrite ("output_images\outout_3_mirror.jpg" , fram)
 
     elif cv2.waitKey (25) & 0xFF == ord ('q') :
         break
